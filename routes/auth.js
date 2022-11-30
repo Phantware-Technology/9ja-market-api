7553983bfec3518e0e60f0e472788c4d1aa8d505
phantware <phantmoney2011@gmail.com>
2022-11-30 20:55:27 +0100

    auth route defined

---
+++ routes/auth.js
@@ -0,0 +1,32 @@
+import User from '../models/User.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+import Joi from 'joi'
+import express from 'express'
+
+const router = express.Router()
+
+router.post('/register', async (res, req) => {
+  const schema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().min(3).max(200).required().email(),
+    password: Joi.string().min(6).max(200),
+  })
+
+  const { error } = schema.valid(req.body)
+
+  if (error) return res.status(400).send(error.data[0].message)
+
+  let user = await User.findOne({ email: req.body.email })
+  if (user) return res.status(400).send('User already exist')
+
+  const salt = bcrypt.genSaltSync(10)
+  const hash = bcrypt.hashSync(req.body.password, salt)
+
+  user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: hash,
+  })
+  await user.save()
+})
