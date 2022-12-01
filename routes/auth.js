93e770b902bffd65c0e118b1586b5bc2d9e72e31
phantware <phantmoney2011@gmail.com>
2022-12-01 23:31:58 +0100

    token added to register route

--- routes/auth.js
+++ routes/auth.js
@@ -30,7 +30,11 @@ router.post('/register', async (req, res) => {
       password: hash,
     })
     await user.save()
-    return res.status(200).send('User has been created.')
+    const token = jwt.sign(
+      { id: user._id, name: user.name, email: user.email },
+      process.env.JWT
+    )
+    return res.status(200).json(token)
   } catch (error) {
     return res.status(500).json(error)
   }
